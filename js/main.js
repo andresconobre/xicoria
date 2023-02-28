6d72a90215709fe46dab04332bb2f8bd42ab2557
andresconobre <75507786+andresconobre@users.noreply.github.com>
2023-02-28 10:54:19 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -129,3 +129,10 @@ tsParticles.load("tsparticles", {
     }
   }
 });
+
+var video = document.getElementById("video"); 
+    //this did the trick
+    video.loop = false; 
+    video.addEventListener('ended', function() { 
+      video.currentTime=0.1; video.play(); }, false);
+    video.play();
